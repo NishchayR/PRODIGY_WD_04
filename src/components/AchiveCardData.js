02fa542b701e6434653e65bd23b2494299921f18
Nishchay <nishchayrajpal8@gmail.com>
2024-01-02 13:15:19 +0530

    updates

--- src/components/AchiveCardData.js
+++ src/components/AchiveCardData.js
@@ -1,6 +1,9 @@
 import glpb from "../assets/glpb.jpg"
 import pg from "../assets/prodigy.jpg"
 import py from "../assets/python.png"
+import tb from "../assets/tableau.jpg"
+import pb from "../assets/powerbi.jpg"
+import exp from "../assets/exp.png"
 
 
 
@@ -19,7 +22,25 @@ const ProjectCardData = [
     },
     {
         imgsrc: pg,
-        title: "Web Development Internship",
+        title: "Web Development Internship @ Prodigy InfoTech",
+        text: "",
+        view: "https://olympus.mygreatlearning.com/courses/30824/certificate",
+    },
+    {
+        imgsrc: tb,
+        title: "Tableau Workshop",
+        text: "",
+        view: "https://olympus.mygreatlearning.com/courses/30824/certificate",
+    },
+    {
+        imgsrc: pb,
+        title: "Power BI Workshop",
+        text: "",
+        view: "https://www.coursera.org/account/accomplishments/verify/C4AGN7GHWCJD",
+    },
+    {
+        imgsrc: exp,
+        title: "Web Development Internship @ Exposys Data Labs",
         text: "",
         view: "https://olympus.mygreatlearning.com/courses/30824/certificate",
     },
